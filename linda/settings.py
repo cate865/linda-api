cb050154f1ffb1dac93a72b39ecf463025450654
Catherine Manyallah <c.muthoni@alustudent.com>
2023-04-24 15:35:55 +0300

    Changed database

--- linda/settings.py
+++ linda/settings.py
@@ -84,11 +84,14 @@ WSGI_APPLICATION = 'linda.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'linda_db', 
+        'USER': 'linda_db_user', 
+        'PASSWORD': '9YUtcq6uDmZx7qvdrWMtq0PkJNjokl53',
+        'HOST': 'dpg-ch37292ut4m9tsbukbv0-a.oregon-postgres.render.com', 
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
